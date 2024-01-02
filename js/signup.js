86c89d375cbf6c1f448490ad14a30bcc974abcbb
Keith Flynn <flynn.keith@gmail.com>
2024-01-02 15:31:46 -0500

    Add form verification JavaScript
    I started this on one OS and am committing it to pull in for working in
    another.

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,10 @@
 console.log("yo yo yo");
+
+const passField1 = document.getElementById("password1");
+const passField2 = document.getElementById("password2");
+
+function checkPasswords() {
+  if (passField1 != passField2) {
+    return;
+  }
+}
