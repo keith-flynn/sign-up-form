aeeb25a570f9362d1d49fbf489040e2cde348409
Keith Flynn <flynn.keith@gmail.com>
2024-01-04 17:18:27 -0500

    Add indicator for non-matching passwords
    Incomplete. CSS class needs added to change border color.

--- js/signup.js
+++ js/signup.js
@@ -4,7 +4,8 @@ const passField1 = document.getElementById("password1");
 const passField2 = document.getElementById("password2");
 
 function checkPasswords() {
-  if (passField1 != passField2) {
-    return;
+  if (passField1.value != passField2.value) {
+    passField1.classList.add("mismatch");
+    passField1.classList.add("mismatch");
   }
 }
