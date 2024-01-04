bc90734867c1dc1a3fdbdd4c2ee0ed2104dd5d6d
Keith Flynn <flynn.keith@gmail.com>
2024-01-04 18:03:55 -0500

    Add non-matching password alert text below field
    Add paragraph to HTML with text already inside. Add CSS styles to p to
    not display normally. Add line to checkPasswords function to change
    display upon non-matching passwords.

--- js/signup.js
+++ js/signup.js
@@ -1,12 +1,14 @@
 const passField1 = document.getElementById("password1");
 const passField2 = document.getElementById("password2");
-const createButton = document.getElementById("create")
+const mismatchText = document.getElementById("mismatch-text")
+const createButton = document.getElementById("create");
 
 // Do our two password fields match?
 function checkPasswords() {
   if (passField1.value != passField2.value) {
     passField1.classList.add("mismatch");
     passField2.classList.add("mismatch");
+    mismatchText.style.display = "block"
   }
 }
 
