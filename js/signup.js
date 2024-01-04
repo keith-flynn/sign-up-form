98fab25c8eb43b467a244cd14ceb07635aea182d
Keith Flynn <flynn.keith@gmail.com>
2024-01-04 17:41:58 -0500

    Add red border for non-matching passwords
    Add button id for button event listener in HTML. Add mismatch class CSS
    decoration red outline. Add event listener for Create Account button,
    and correct checkPasswords function to target both password fields.

--- js/signup.js
+++ js/signup.js
@@ -1,11 +1,13 @@
-console.log("yo yo yo");
-
 const passField1 = document.getElementById("password1");
 const passField2 = document.getElementById("password2");
+const createButton = document.getElementById("create")
 
+// Do our two password fields match?
 function checkPasswords() {
   if (passField1.value != passField2.value) {
     passField1.classList.add("mismatch");
-    passField1.classList.add("mismatch");
+    passField2.classList.add("mismatch");
   }
 }
+
+createButton.addEventListener("click", checkPasswords);
